022feed1c2dd9326c4ff726afc6923c0d8da9344
qq7337 <1590747796@qq.com>
2025-03-19 20:12:06 +0800

    更新
    用户信息界面

--- src/views/LeftBodyDiv/LeftBDiv2.vue
+++ src/views/LeftBodyDiv/LeftBDiv2.vue
@@ -1,13 +1,11 @@
 <template>
-  <el-tree
+ <el-tree
     style="max-width: 600px"
     :data="data"
     :props="defaultProps"
     @node-click="handleNodeClick"
-    class="custom-tree"
   />
 </template>
-
 <script setup>
 const handleNodeClick = (data) => {
   console.log(data);
@@ -15,24 +13,28 @@ const handleNodeClick = (data) => {
 
 const data = [
   {
-    label: '应用商店',
+    label: 'Level one 1',
     children: [
       {
-        label: '最近更新',
-      },
-      {
-        label: '效率工具',
-      },
-      {
-        label: '代码助手',
+        label: 'Level two 1-1',
+        children: [
+          {
+            label: 'Level three 1-1-1',
+          },
+        ],
       },
     ],
   },
   {
-    label: '分类查看',
+    label: 'Level one 2',
     children: [
       {
         label: 'Level two 2-1',
+        children: [
+          {
+            label: 'Level three 2-1-1',
+          },
+        ],
       },
       {
         label: 'Level two 2-2',
@@ -45,7 +47,7 @@ const data = [
     ],
   },
   {
-    label: '我的收藏',
+    label: 'Level one 3',
     children: [
       {
         label: 'Level two 3-1',
@@ -72,42 +74,3 @@ const defaultProps = {
   label: 'label',
 };
 </script>
-
-<style >
-/* 全局样式 */
-.custom-tree {
-  font-size: 16px;
-  color: #333;
-  font-weight: 600;
-  background-color: #ffffff;
-  /* border: 1px solid #e4e7ed; */
-  border-radius: 8px;
-  padding: 10px;
-}
-
-.custom-tree .el-tree-node__content {
-  height: 40px !important;
-  line-height: 40px !important;
-  margin-bottom: 5px;
-  border-radius: 4px;
-  transition: background-color 0.3s ease;
-}
-
-.custom-tree .el-tree-node__content:hover {
-  background-color: #ffffff !important;
-}
-
-.custom-tree .el-tree-node__expand-icon {
-  font-size: 16px !important;
-  color: #666 !important;
-}
-
-.custom-tree .el-tree-node__expand-icon.is-leaf {
-  color: transparent !important;
-}
-
-.custom-tree .el-tree-node.is-current > .el-tree-node__content {
-  background-color: #b3e5fc !important;
-  color: #1890ff !important;
-}
-</style>
--- src/views/LefyBody.vue
+++ src/views/LefyBody.vue
@@ -1,118 +1,121 @@
 <template>
   <div class="lefybodybody">
-    <div class="lefybody">
-      <div class="leftbody2">
-        <img class="clickable fclass" width="30px" src="../icons/50309213222.png" />
-        <img class="fclass2 clickable" width="30px" src="../icons/chuangjian.png" />
-        <hr />
-        <div @click="handleview0" :class="['fc3body', { fc3bodyover: view === 0 }]" class="clickable">
-          <img class="fclass3" width="20px" src="../icons/zhuye.png" />
-          <p>主页</p>
-        </div>
-        <div @click="handleview1" :class="['fc3body', { fc3bodyover: view === 1 }]" class="clickable">
-          <img class="fclass3" width="20px" src="../icons/duowenjianjia.png" />
-          <p>工作空间</p>
-        </div>
-        <div @click="handleview2" :class="['fc3body', { fc3bodyover: view === 2 }]" class="clickable">
-          <img class="fclass3" width="20px" src="../icons/shangdian.png" />
-          <p>商店</p>
-        </div>
-        <div class="fc3body clickable">
-          <img class="fclass3" width="20px" src="../icons/mobankuangjia-xianxing.png" />
-          <p>模板</p>
-        </div>
-        <div class="fc3body clickable">
-          <img class="fclass3" width="20px" src="../icons/api-fill.png" />
-          <p>山清API</p>
-        </div>
-        <div class="bodyl">
-          <div class="last1 clickable">
-            <img width="20px" src="../icons/shiyongwendang.png" />
-          </div>
-          <div class="last1 clickable">
-            <img width="20px" src="../icons/xiaoxi.png" />
-          </div>
-          <div class="last1 clickable">
-            <img width="20px" src="../icons/yijiangoumai.png" />
-          </div>
-          <div class="last1 clickable">
-            <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
-          </div>
-        </div>
-      </div>
-      <div ref="rightbody" class="rightbody">
-        <div v-if="0" @click="handleright" class="hiddenicon"></div>
-        <LeftBDiv0 v-if="view === 0" />
-        <LeftBDiv2 v-if="view === 2" />
-        <LeftBDiv1 v-if="view === 1" />
-      </div>
-    </div>
+ <div class="lefybody">
+<div class="leftbody2">
+<img class="clickable fclass" width="30px" src="../icons/50309213222.png">
+<img class="fclass2 clickable" width="30px" src="../icons/chuangjian.png">
+<hr>
+<div @click="handleview0" class="fc3body fc3bodyover clickable">
+<img class="fclass3 " width="20px" src="../icons/zhuye.png">
+<p>主页</p>
+
+</div>
+<div class="fc3body clickable">
+<img class="fclass3" width="20px" src="../icons/duowenjianjia.png">
+<p>工作空间</p>
+
+</div>
+<div @click="handleview2"  class="fc3body clickable">
+<img class="fclass3" width="20px" src="../icons/shangdian.png">
+<p>商店</p>
+
+</div>
+<div class="fc3body clickable">
+<img class="fclass3" width="20px" src="../icons/mobankuangjia-xianxing.png">
+<p>模板</p>
+
+</div>
+<div class="fc3body clickable">
+<img class="fclass3" width="20px" src="../icons/api-fill.png">
+<p>山清API</p>
+
+</div>
+<div class="bodyl">
+  <div class="last1 clickable">
+<img  width="20px" src="../icons/shiyongwendang.png">
+
+</div>
+  <div class="last1 clickable">
+<img  width="20px" src="../icons/xiaoxi.png">
+
+</div>
+  <div class="last1 clickable">
+<img  width="20px" src="../icons/yijiangoumai.png">
+
+</div>
+  <div class="last1 clickable">
+  <el-avatar
+       @click="router.push('/user')"
+       
+        src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+      />
   </div>
+
+
+
+</div>
+
+
+
+
+</div>
+<div  ref="rightbody" class="rightbody">
+      <div v-if="0" @click="handleright" class="hiddenicon"></div>
+      <LeftBDiv0 v-if="view == 0" />
+      <LeftBDiv2 v-if="view == 2" />
+    </div>
+
+
+  </div>
+  </div>
+
 </template>
 
 <script setup>
-import LeftBDiv0 from './LeftBodyDiv/LeftBDiv0.vue';
-import LeftBDiv1 from './LeftBodyDiv/LeftBDiv1.vue';
-import LeftBDiv2 from './LeftBodyDiv/LeftBDiv2.vue';
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-// 从 sessionStorage 中读取 view 的值，如果没有则默认为 0
-const view = ref(parseInt(sessionStorage.getItem('view')) || 0);
-
-// 页面加载时读取 sessionStorage 中的 view 值
-onMounted(() => {
-  const savedView = sessionStorage.getItem('view');
-  if (savedView !== null) {
-    view.value = parseInt(savedView);
-    updateActiveClass(view.value);
-  }
-});
-
-// 更新激活的菜单项样式
-const updateActiveClass = (index) => {
+import LeftBDiv0 from './LeftBodyDiv/LeftBDiv0.vue'
+import LeftBDiv2 from './LeftBodyDiv/LeftBDiv2.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+let view = ref(0)
+const handleview0 = () => {
+  // 获取所有class为fc3body的div元素
   const divs = document.querySelectorAll('.fc3body');
-  divs.forEach((div, i) => {
+  view.value = 0;
+  // 遍历所有div
+  divs.forEach((div, index) => {
+    // 移除所有div的fc3bodyover类
     div.classList.remove('fc3bodyover');
-    if (i === index) {
+    // 为第1个div添加fc3bodyover类
+    if (index === 0) { // 索引从0开始，1表示第二个元素
       div.classList.add('fc3bodyover');
     }
   });
-};
 
-// 处理主页点击
-const handleview0 = () => {
-  view.value = 0;
-  sessionStorage.setItem('view', 0);
-  updateActiveClass(0);
+  // 路由跳转
   router.push('/');
 };
-
-// 处理工作空间点击
-// 处理工作空间点击
-const handleview1 = () => {
-  view.value = 1;
-  sessionStorage.setItem('view', 1);
-  updateActiveClass(1);
-  console.log('Before router.push');
-  router.push('/SoftwareManagement').then(() => {
-    console.log('After router.push');
-  }).catch((error) => {
-    console.error('Router push error:', error);
-  });
-};
-
-// 处理商店点击
 const handleview2 = () => {
+  // 获取所有class为fc3body的div元素
+  const divs = document.querySelectorAll('.fc3body');
   view.value = 2;
-  sessionStorage.setItem('view', 2);
-  updateActiveClass(2);
-  router.push('/store');
-};
 
-const rightbody = ref(null);
+  // 遍历所有div
+  divs.forEach((div, index) => {
+    // 移除所有div的fc3bodyover类
+    div.classList.remove('fc3bodyover');
+    // 为第3个div添加fc3bodyover类
+    if (index === 2) { // 索引从0开始，1表示第二个元素
+      div.classList.add('fc3bodyover');
+    }
+  });
+
+  // 路由跳转
+  router.push('/store');
+
+}
+
+const rightbody = ref(null); // 使用 ref 获取 DOM 元素
 
 const handleright = () => {
   console.log('11');
@@ -125,24 +128,24 @@ const handleright = () => {
 </script>
 
 <style scoped>
-/* 样式保持不变 */
-.visiable {
+.visiable{
   z-index: 2;
   width: 15px;
   height: 15px;
-  background-image: url('../icons/zhankai.png');
+  background-image: url("../icons/zhankai.png");
   background-size: 100% 100%;
 }
-.hiddenicon {
+.hiddenicon{
   width: 15px;
   height: 15px;
-  background-image: url('../icons/shouqi.png');
+  background-image: url("../icons/shouqi.png");
   background-size: 100% 100%;
   position: absolute;
   bottom: 48vh;
   right: 10px;
+  /* background-color: #757a8c; */
 }
-.rightbody {
+.rightbody{
   width: 200px;
   z-index: 10;
   border: #e7e7ec 1px solid;
@@ -154,63 +157,66 @@ const handleright = () => {
   background-color: #fff;
   height: 100%;
 }
-.last1:hover {
+.last1:hover{
   background-color: #f2f3f8;
   border-radius: 10px;
 }
-.last1 {
-  height: 40px;
+.last1{
+height: 40px;
   width: 40px;
   margin: 0px auto;
-  margin-bottom: 20px;
+margin-bottom: 20px;
+
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.bodyl {
-  position: absolute;
+.bodyl{
+  position:absolute;
   bottom: 0px;
   background-color: #ffffff;
   width: 58px;
 }
-.fc3body:hover {
+
+.fc3body:hover{
   background-color: #f2f3f8;
 }
-.fc3bodyover {
+.fc3bodyover{
   background-color: #f2f3f8;
 }
-.fc3body {
-  width: 50px;
-  height: 48px;
-  color: #757a8c;
-  display: flex;
-  flex-direction: column;
-  font-size: 10px;
-  margin-top: 10px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 5px;
+.fc3body{
+width: 50px;
+height: 48px;
+color: #757a8c;
+display: flex;
+flex-direction: column;
+font-size: 10px;
+margin-top: 10px;
+justify-content: center;
+align-items: center;
+border-radius: 5px;
 }
-hr {
+
+hr{
   width: 60%;
   height: 1px;
   margin-top: 20px;
   border: #e7e7ec 1px solid;
 }
-.fclass2 {
+.fclass2{
   margin-top: 30px;
   border-radius: 10px;
 }
-.fclass {
+.fclass{
   margin-top: 20px;
   border-radius: 10px;
 }
-.leftbody2 {
+.leftbody2{
   display: flex;
   flex-direction: column;
   overflow: hidden;
   background-color: #fff;
-  border-radius: 20px;
+border-radius: 20px;
   justify-content: first baseline;
   align-items: center;
   width: 60px;
@@ -218,16 +224,17 @@ hr {
   height: 100%;
   border-right: 1px solid #e7e7ec;
 }
-.lefybodybody {
+.lefybodybody{
   width: auto;
   height: 100vh;
   display: flex;
   align-items: center;
 }
-.lefybody {
+.lefybody{
   width: auto;
   margin-left: 10px;
   border-radius: 20px;
   height: 98vh;
 }
+
 </style>
